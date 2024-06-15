52876bc089e3922a5ca5326251d091fefcacd1fe
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2024-06-15 10:40:05 +0300

    Changed FixedDT

--- scripts/Utils/Time.js
+++ scripts/Utils/Time.js
@@ -1,6 +1,6 @@
 export class Time {
     static get DeltaTime() { return this.deltaTime; }
-    static get FixedDeltaTime() { return 0.005; }
+    static get FixedDeltaTime() { return 0.002; }
     static get FPS() { return Math.round(1 / this.deltaTime); }
     static Start(currentTime) {
         this.oldTime = currentTime;
